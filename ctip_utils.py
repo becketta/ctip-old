6d18140ddf7c1edbebb5da9dee493a3cee156850
Aaron Beckett <becketta@msu.edu>
2016-01-25 17:03:44 -0500

    Fill in a few more utility functions.

--- ctip_utils.py
+++ ctip_utils.py
@@ -3,43 +3,56 @@
 #
 
 import sqlite3 as sql
+import csv
 
 class CTIPDatabaseManager:
 
     dbname = "test.db"
 
     def __init__(self):
-        pass
+        self.conn = sql.connect(self.dbname)
+        self.conn.row_factory = sql.Row
 
-    def storeSnapshot(self, table, outfilename):
+    def __del__(self):
+        self.conn.close()
+
+    def storeSnapshot(self, table, outfilename=None):
         """Store the contents of a config table as a csv file."""
-        pass
+        cols,types,configs = self.getConfigs(table)
+
+        if not outfilename:
+            outfilename = str(table) + ".csv"
+
+        with open(outfilename, 'w') as summary:
+            writer = csv.writer(summary)
+            writer.writerow([table])
+            writer.writerow(cols)
+            writer.writerow(types)
+            for config in configs:
+                writer.writerow(list(config))
+
 
     def listConfigTables(self):
         """List the names of all tables in the database."""
-        pass
+        query = "SELECT name FROM sqlite_master WHERE type='table'"
+        for row in self.conn.execute(query):
+            print row[0]
 
     def getConfigs(self, table, whereClause=""):
+
+        infoquery = "PRAGMA table_info(" + table + ")"
+        configquery = "SELECT * FROM " + table + " " + whereClause
+
+        # Get the column info
         colnames = []
         coltypes = []
-        configs = []
+        for col in self.conn.execute(infoquery):
+            colnames.append(col[1])
+            coltypes.append(col[2])
 
-        conn = sql.connect(self.dbname)
-        with conn:
-
-            conn.row_factory = sql.Row
-
-            infoquery = "PRAGMA table_info(" + table + ")"
-            configquery = "SELECT * FROM " + table + " " + whereClause
-
-            # Get the column info
-            for col in conn.execute(infoquery):
-                colnames.append(col[1])
-                coltypes.append(col[2])
-
-            # Get the relevant configurations
-            #   -> if whereClause is "", get all the configs in the table
-            configs = conn.execute(configquery).fetchall()
+        # Get the relevant configurations
+        #   -> if whereClause is "", get all the configs in the table
+        configs = self.conn.execute(configquery).fetchall()
 
         return colnames,coltypes,configs
 
