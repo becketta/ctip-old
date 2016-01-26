8eb238025971624cdd7dc4c65e76e1c75539858a
Aaron Beckett <becketta@msu.edu>
2016-01-26 17:29:23 -0500

    Moved most ctip functionality into ctip_utils, left parsing and runConfig in ctip.

--- ctip_utils.py
+++ ctip_utils.py
@@ -3,9 +3,16 @@
 #
 
 import sqlite3 as sql
+import os
 import csv
+import datetime
+from multiprocessing import Process
 
-class CTIPDatabaseManager:
+###########################################################
+#   Utility Classes
+#
+
+class DatabaseManager:
 
     dbname = "test.db"
 
@@ -15,19 +22,6 @@ class CTIPDatabaseManager:
 
     def __del__(self):
         self.conn.close()
-
-    def storeSnapshot(self, table, outfile):
-        """Store the contents of a config table as a csv file."""
-        cols,types,configs = self.getConfigs(table)
-        writeConfigCsv(outfile, table, cols, types, configs)
-        
-    def writeConfigCsv(self, outfile, tablename, cols, types, configs):
-        writer = csv.writer(outfile)
-        writer.writerow([tablename])
-        writer.writerow(cols)
-        writer.writerow(types)
-        for config in configs:
-            writer.writerow(list(config))
 
     def listConfigTables(self):
         """List the names of all tables in the database."""
@@ -80,50 +74,113 @@ class CTIPDatabaseManager:
 
         return colnames,coltypes,configs
 
-    def createConfigTable(self, csv_file):
-        """Creates new config table filled with the csv file configs"""
-        configTableName = ""
-        reader = csv.reader(csv_file)
+    def addConfigTable(self, name, colnames, coltypes, configs):
+        pass
 
-        # Get the name of this group of configs
-        row = next(reader)
-        if len(row) == 1:
-            configTableName = row[0]
-        else:
-            e = "First line of csv file must be the name of this \
-                    group of configs"
-            raise RuntimeError(e)
 
-        # Get the column names and types
-        cols = next(reader)
-        types = next(reader)
+###########################################################
+#   Utility Functions
+#
 
-        # Zip the column names to the column types
-        if len(cols) != len(types):
-            e = "Line 2 and 3 must contain column names and data types \
-                    respectively"
-            raise RuntimeError(e)
-        else:
-            colinfo = zip(cols,types)
+def storeSnapshot(table, outfile):
+    """Store the contents of a config table as a csv file."""
+    db = DatabaseManager()
+    cols,types,configs = db.getConfigs(table)
+    writeConfigCsv(outfile, table, cols, types, configs)
+    
+def writeConfigCsv(outfile, tablename, cols, types, configs):
+    """Write the given table info in csv format."""
+    writer = csv.writer(outfile)
+    writer.writerow([tablename])
+    writer.writerow(cols)
+    writer.writerow(types)
+    for config in configs:
+        writer.writerow(list(config))
+        
+def createConfigTable(csv_file):
+    """Creates new config table from a properly formatted csv file."""
+    configTableName = ""
+    reader = csv.reader(csv_file)
 
-        return configTableName
+    # Get the name of this group of configs
+    row = next(reader)
+    if len(row) == 1:
+        configTableName = row[0]
+    else:
+        e = "First line of csv file must be the name of this \
+                group of configs"
+        raise RuntimeError(e)
 
-    def generateConfigTable(self, csv_file):
-        """
-        Generates new config table of all combinations of csv config values
-        """
-        configTableName = ""
-        reader = csv.reader(csv_file)
+    # Get the column names and types
+    cols = next(reader)
+    types = next(reader)
 
-        # Get the name of this group of configs
-        row = next(reader)
-        if len(row) == 1:
-            configTableName = row[0]
-        else:
-            e = "First line of csv file must be the name of this \
-                    group of configs"
-            raise RuntimeError(e)
+    # Zip the column names to the column types
+    if len(cols) != len(types):
+        e = "Line 2 and 3 must contain column names and data types \
+                respectively"
+        raise RuntimeError(e)
+    else:
+        colinfo = zip(cols,types)
 
-        return configTableName
+    return configTableName
+
+def generateConfigTable(csv_file):
+    """Generates config table from csv file of valid config parameters."""
+    configTableName = ""
+    reader = csv.reader(csv_file)
+
+    # Get the name of this group of configs
+    row = next(reader)
+    if len(row) == 1:
+        configTableName = row[0]
+    else:
+        e = "First line of csv file must be the name of this \
+                group of configs"
+        raise RuntimeError(e)
+
+    return configTableName
+
+def initTestSession(test_func, table, whereClause="", outdir=""):
+    """
+    Initialize a test session of all configs in 'table' that satisfy
+    the 'whereClause'.
+    """
+
+    manager = DatabaseManager()
+
+    # Get configs from the database
+    colnames,coltypes,configs = manager.getConfigs(table, whereClause)
+
+    #
+    # Create a folder for this test session based on the date and time
+    #   -> The directory being created is:
+    #           <outDir>/<configTable>/<date_time>/
+    #   -> Each config will create a folder in this directory
+    #
+    now = datetime.datetime.now()
+    sec = now.second
+    timestamp = now.strftime("%Y-%m-%d_%H:%M:") + str(sec)
+
+    testBatchDir = os.path.join(outdir, table, timestamp)
+    try:
+        os.makedirs(testBatchDir)
+    except OSError:
+        if not os.path.isdir(testBatchDir):
+            raise RuntimeError('Could not create the output directory')
+
+    # Store text file snapshot of config table at root session dir
+    snapshotPath = os.path.join(testBatchDir, "configs.csv")
+    with open(snapshotPath, 'w') as sf:
+        writeConfigCsv(sf, table, colnames, coltypes, configs)
+
+    # Spawn processes for each config to test
+    test_processes = []
+    for config in configs:
+        p = Process(target=test_func, args=(config, testBatchDir))
+        p.start()
+        test_processes.append(p)
+
+    return test_processes
 
 
