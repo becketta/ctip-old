d87b5ceb17b07c081586999380dfc1172cb47917
Aaron Beckett <becketta@msu.edu>
2016-04-11 23:57:54 -0400

    Tailor runConfig function to P3Brain.

--- example_run_config.py
+++ example_run_config.py
@@ -6,13 +6,15 @@ from subprocess import Popen, PIPE
 from multiprocessing import Queue
 
 from ctip_utils import QsubBuilder
-from ctip_constants import QSUB_TEMPLATE, CFG_TEMPLATE
+from ctip_constants import CTIP_ROOT
 
 ################################################################
 # Set this variable to the name of your templated qsub file
 ################################################################
+templated_qsub_file = CTIP_ROOT + "example_qsub.qsub"
 
-def runConfig(config, queue, outdir="", qsub_file=QSUB_TEMPLATE):
+
+def runConfig(config, queue, outdir="", qsub=templated_qsub_file):
     """Run a program using given configuration"""
 
     # Build the directory for this run
@@ -31,39 +33,33 @@ def runConfig(config, queue, outdir="", qsub_file=QSUB_TEMPLATE):
     runDir = os.path.join(outdir, runName)
     os.makedirs(runDir)
 
-
-
+    # Generate the config file name for this run
     cfg_file = os.path.join(runDir, runName + ".cfg")
+
 
     ################################################################
     # EDIT CREATION OF CONFIG FILE:
     ################################################################
 
-    subst_dict = {
-        'job_dir': runDir,
-        'world': config['world'],
-        'decoder': config['decoder'],
-        'gates': config['gates'],
-        'gate_complexity': config['gate_complexity'],
-        'trials': config['trials'],
-        'length': config['length'],
-        'optimizer': config['optimizer'],
-        'eval_limit': config['eval_limit'],
-        'fitness_limit': config['fitness_limit']
-    }
+    # Fill lines list with lines to add to EVERY config file
+    out_file_name = os.path.join(runDir, "ascii_images")
+    lines = [ "out_file {0}\n".format(out_file_name) ]
+
+    # Loop over all columns in the config
+    for key in config.keys():
+        # Skip the special cols of 'id' and 'tag' that are used by ctip
+        if key not in ('id', 'tag', 'wait_time'):
+            # Create a formated line to add to the config file for
+            # this key, value pair
+            lines.append("{0} {1}\n".format(key, config[key]))
 
     ################################################################
 
 
 
-    # Create custom string Template from the cfg template
-    with open(CFG_TEMPLATE, 'r') as cfg_template:
-        template = QsubBuilder(cfg_template.read())
-
-    # Create the config file from the template using subst_dict
+    # Write the lines list to the config file
     with open(cfg_file, 'w') as cfg:
-        cfg_text = template.substitute(subst_dict)
-        cfg.write(cfg_text)
+        cfg.writelines(lines)
 
 
 
@@ -73,32 +69,33 @@ def runConfig(config, queue, outdir="", qsub_file=QSUB_TEMPLATE):
 
     # Match values to all keys that are in the qsub template
     subst_dict = {
-        # Should probably leave these mappings as is
-        'job_name': runName,
-        'shell_out_file': os.path.join(runDir, runName + ".o"),
-        'job_dir': runDir,
+            # Should probably leave these mappings as is
+            'job_name': runName,
+            'shell_out_file': os.path.join(runDir, runName + ".o"),
+            'job_dir': runDir,
 
-        # May want to edit these mappings and add your own, just
-        # make sure there's a corresponding %=<key_name> in the
-        # qsub template. For example, the below mapping replaces
-        # all '%=config_file' strings in the qsub template with
-        # the string stored in cfg_file
-        'config_file': cfg_file,
-    }
+            # May want to edit these mappings and add your own, just
+            # make sure there's a corresponding %=<key_name> in the
+            # qsub template. For example, the below mapping replaces
+            # all '%=config_file' strings in the qsub template with
+            # the string stored in cfg_file
+            'config_file': cfg_file,
+            'wait_time': config['wait_time']
+        }
 
     ################################################################
 
 
 
     # Create custom string Template from the qsub template
-    with open(qsub_file, 'r') as qsub_template:
+    with open(qsub, 'r') as qsub_template:
         template = QsubBuilder(qsub_template.read())
 
-    # Create the qsub file from the template using subst_dict
+    # Create the temporary qsub file from the template using subst_dict
     run_qsub = os.path.join(runDir, runName + ".qsub")
-    with open(run_qsub, 'w') as qsub:
+    with open(run_qsub, 'w') as temp_qsub:
         qsub_text = template.substitute(subst_dict)
-        qsub.write(qsub_text)
+        temp_qsub.write(qsub_text)
 
     #
     # Submit the job to the scheduler using the temporary qsub file
